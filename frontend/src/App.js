e699f80e55f63fa5131e9c21e23b88c1c0d615ca
IsaacP <isaac@pachon.com>
2024-05-08 23:17:51 -0500

    Vista del Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom';
+import LoginForm from './Components/LoginForm/LoginForm';
+import Home from './Pages/Home/Home'
 import './App.css';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    return <Navigate to='/Home' />;  
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          {/* Ruta para el formulario de inicio de sesión */}
+          <Route
+            path='/'
+            element={<LoginForm handleLogin={handleLogin} />}
+          />
+          {/* Ruta protegida para el Home */}
+          <Route
+            path='/Home'
+            element={isLoggedIn ? <Home /> : <Navigate to='/Home' />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
