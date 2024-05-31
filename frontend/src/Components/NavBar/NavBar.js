9a8f294b0924c6732b1599df12083e3ab1d6eb05
m4r14j0 <mariajose@esvyda.com>
2024-05-30 21:46:35 -0500

    added new api route on front

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,41 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
-import { FaSearch, FaPaperPlane, FaArrowRight } from 'react-icons/fa';
+import { FaSearch } from 'react-icons/fa';
 import { IoMdExit } from "react-icons/io";
 import { IoSend } from "react-icons/io5";
 import { Link, useNavigate } from 'react-router-dom';
 
-
 const Navbar = ({ onSearchClick }) => {
+  const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem('user');
     console.log('Sesión cerrada');
-    navigate ('/Login');
+    navigate('/Login');
   };
-  
-  const handleSearchFocus = () => {
-    onSearchClick();
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    console.log('Búsqueda:', searchQuery);
+    onSearchClick(searchQuery);
   };
 
   return (
     <div className="navbar-container">
       <div className="app-name">
-        <Link to ="/Home">
+        <Link to="/Home">
           <h2>LIGANET</h2>
         </Link>
       </div>
       <div className="search-container">
         <FaSearch className="search-icon" />
-        <input className="search-input" placeholder="Buscar..." onFocus={handleSearchFocus} />
-        <button className="search-button">
+        <input 
+          className="search-input" 
+          placeholder="Buscar..." 
+          value={searchQuery} 
+          onChange={handleSearchChange} 
+        />
+        <button className="search-button" onClick={handleSearchSubmit}>
           <IoSend />
         </button>
       </div>
       <div className='right-icons'>
         <img className="profile-image" src="https://res.cloudinary.com/dmmnud7ix/image/upload/v1717051319/inyfu0ynjvbao60ktlfb.jpg" alt="Perfil" />
-        <IoMdExit className="exit-icon" onClick={handleLogout}/>
+        <IoMdExit className="exit-icon" onClick={handleLogout} />
       </div>
     </div>
   );
--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '../../Components/NavBar/NavBar';
 import SideBar from '../../Components/SideBar/SideBar';
 import HomeView from '../HomeView/HomeView';
 import SearchView from '../SearchView/SearchView';
 import FilterBar from '../../Components/FilterBar/FilterBar';
+import { fetchJugadores, fetchTorneos, fetchEquipos, fetchCanchas } from '../../Services/search';
 import './Home.css';
 
 const Home = () => {
-
   const [view, setView] = useState('home'); // 'home' or 'search'
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filter, setFilter] = useState('');
+  const [jugadores, setJugadores] = useState([]);
+  const [torneos, setTorneos] = useState([]);
+  const [equipos, setEquipos] = useState([]);
+  const [canchas, setCanchas] = useState([]);
 
-  const handleSearchClick = () => {
+  const handleSearchClick = (query) => {
+    console.log('Búsqueda en Home:', query);
+    setSearchQuery(query);
     setView('search');
   };
 
@@ -21,24 +29,78 @@ const Home = () => {
 
   const handleResetFilters = () => {
     console.log('Filtros restablecidos');
-    // Aquí puedes agregar la lógica para restablecer los filtros
+    setFilter('');
+    fetchAllData();
   };
+
+  const handleFilterClick = (filter) => {
+    setFilter(filter);
+    fetchFilteredData(filter, searchQuery);
+  };
+
+  const fetchAllData = async () => {
+    const [jugadoresData, torneosData, equiposData, canchasData] = await Promise.all([
+      fetchJugadores(),
+      fetchTorneos(),
+      fetchEquipos(),
+      fetchCanchas()
+    ]);
+    setJugadores(jugadoresData);
+    setTorneos(torneosData);
+    setEquipos(equiposData);
+    setCanchas(canchasData);
+  };
+
+  const fetchFilteredData = async (filter, query) => {
+    if (filter === 'jugadores') {
+      setJugadores(await fetchJugadores(query));
+      setTorneos([]);
+      setEquipos([]);
+      setCanchas([]);
+    } else if (filter === 'torneos') {
+      setJugadores([]);
+      setTorneos(await fetchTorneos(query));
+      setEquipos([]);
+      setCanchas([]);
+    } else if (filter === 'equipos') {
+      setJugadores([]);
+      setTorneos([]);
+      setEquipos(await fetchEquipos(query));
+      setCanchas([]);
+    } else if (filter === 'canchas') {
+      setJugadores([]);
+      setTorneos([]);
+      setEquipos([]);
+      setCanchas(await fetchCanchas(query));
+    }
+  };
+
+  useEffect(() => {
+    if (view === 'search') {
+      fetchFilteredData(filter, searchQuery);
+    }
+  }, [view, searchQuery, filter]);
 
   return (
     <div className="home-container">
       <NavBar onSearchClick={handleSearchClick} />
       <div className="main-content">
         <SideBar onHomeClick={handleHomeClick} />
-          {view === 'home' ? (
-            <div className="content">
+        {view === 'home' ? (
+          <div className="content">
             <HomeView />
           </div>
-          ) : (
-            <div className="content">
-              <FilterBar onReset={handleResetFilters} />
-              <SearchView />
-            </div>
-          )}
+        ) : (
+          <div className="content">
+            <FilterBar onReset={handleResetFilters} onFilter={handleFilterClick} />
+            <SearchView 
+              jugadores={jugadores} 
+              torneos={torneos} 
+              equipos={equipos} 
+              canchas={canchas} 
+            />
+          </div>
+        )}
       </div>
     </div>
   );
